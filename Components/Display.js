6b98e9a9a0c65e93324ee2652e00d4484e09cad6
bhavin295 <bhavin.modi@bacancy.com>
2021-10-24 21:14:34 -0700

    change in forth

--- Components/Display.js
+++ Components/Display.js
@@ -6,7 +6,7 @@ const Display = () => {
   return (
     <div>
       <h1>Button clicked {clickContext.value} times</h1>
-      <h3>This is a change</h3>
+      <h3>This is a modified change</h3>
     </div>
   );
 };
