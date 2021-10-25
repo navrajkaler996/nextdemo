64c0d25a00aab667e1e91eec958ab3a8769c7bb2
bhavin295 <bhavin.modi@bacancy.com>
2021-10-24 20:50:33 -0700

    styled added

--- Components/Display.js
+++ Components/Display.js
@@ -7,7 +7,7 @@ const Display = () => {
     <div>
       <h1>Button clicked {clickContext.value} times</h1>
       <h3>This is a change</h3>
-      <h3>This is another change</h3>
+      <h3 style={{ color: green }}>This is another change</h3>
     </div>
   );
 };
